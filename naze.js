c5c5ced93490180c4dc0b459efa7aedfce501758
Rizaldn <111334720+Rizaldn@users.noreply.github.com>
2022-10-19 12:30:03 +0700

    Update naze.js

--- naze.js
+++ naze.js
@@ -2936,7 +2936,7 @@ let alfamart = `628111500959@s.whatsapp.net`
             let ownernya = ownernomer + '@s.whatsapp.net'
             let me = m.sender
             let ments = [ownernya, me, ini_mark]
-            let kukiw = `*${ucapanWaktu} @${me.split('@')[0]} 🙌*
+            let kukiw = `*${ucapanWaktu} ${pushname} 🙌*
 ╭──❍「 𝙄𝙉𝙁𝙊 𝙐𝙎𝙀𝙍 」❍
 ├ *Nama* : ${pushname}
 ├ *Number* : ${me.split('@')[0]}
